8d6de9dba6924542e43394023cff79146b32cf41
POWERSL2DE <naziksl2de@gmail.com>
2024-07-05 23:57:46 +0300

    Summary
    - App.jsx
    - Description.jsx/module.css
    - Feedback.jsx
    - Notification.jsx
    - Options.jsx/module.css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import Description from '../Description/Description';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
+
+
+
+const App = () => {
+
+    const [feedback, setFeedback] = useState(
+        () => JSON.parse(localStorage.getItem('saved-feedback')) ?? { good: 0, neutral: 0, bad: 0 }
+    );
+
+
+    useEffect(() => {
+        window.localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+    }, [feedback]);
+
+
+    const updateFeedback = feedbackType => {
+        setFeedback({
+            ...feedback,
+            [feedbackType]: feedback[feedbackType] + 1,
+        });
+    };
+
+
+    const resetFeedback = () => {
+        setFeedback({ good: 0, neutral: 0, bad: 0 });
+    };
+
+
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const positiveFeedback = Math.round(((feedback.good + feedback.neutral) / totalFeedback) * 100);
+
+
+    return (
+      <>
+        <Description/>
+        <Options
+            feedback={feedback}
+            onUpdate={updateFeedback}
+            onReset={resetFeedback}
+            total={totalFeedback}
+        />
+
+        <Feedback
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positive={positiveFeedback}
+        />
+
+        <Notification total={totalFeedback}/>
+      </>
+    );
+};
+
+export default App;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,20 @@
+import css from './Options.module.css';
+
+const Options = ({ feedback, onUpdate, onReset, total }) => {
+  return (
+    <>
+      {Object.keys(feedback).map(elem => (
+        <button className={css.button} onClick={() => onUpdate(elem)} key={elem}>
+          {elem}
+        </button>
+      ))}
+      {total > 0 && (
+        <button className={css.button} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Options;
